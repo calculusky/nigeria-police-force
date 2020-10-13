721f02b1c4b4b6d58760ab6f52a6c5f17333f4ab
calculusky <calculusky@gmail.com>
2020-10-13 13:23:17 +0100

    npfabis3

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-exports.npfabisEntryFilePath = 'C:\\LEYSTEL\\software development\\npfabis\\app.js';
+//exports.npfabisEntryFilePath = 'C:\\LEYSTEL\\software development\\npfabis\\app.js';
+exports.npfabisEntryFilePath = 'C:\\NPFABIS\\app.js';
